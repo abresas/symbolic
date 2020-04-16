5c573db2fc1d3b0000eff5bf42559f07e418e11b
Alexios Theodoros Brezas <abresas@gmail.com>
2020-04-16 16:29:21 +0300

    derivative powers ln, e etc

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 from typing import Any
 
@@ -124,7 +125,12 @@ class Constant(Expression):
         """
         >>> print(Constant(2))
         2
+        >>> import math
+        >>> print(Constant(math.e))
+        e
         """
+        if self.value == math.e:
+            return "e"
         return str(self.value)
 
 @dataclass
@@ -158,6 +164,18 @@ class Multiply(Expression):
         6 * x
         >>> print((3 * (x * 2)).simplify())
         6 * x
+        >>> print(((x ** -1) * (x ** 2)).simplify())
+        x
+        >>> print((3 * ((x ** -1) * (x ** 2))).simplify())
+        3 * x
+        >>> print(((3 * (x ** -1)) * (x ** 2)).simplify())
+        3 * x
+
+        * (a ** e1) * (b ** e2) * (a ** e3) * (d ** e4)
+        -> (a ** (e1 + e3)) * (b ** e2) * (d ** e4)
+
+        Multiply((a ** e1), Multiply(b ** e2, (a ** e3, Multiply(d ** e4, a ** e6)))
+        -> Multiply(a ** e1 + e2 + e6, ...)
         """
         left = self.left.simplify()
         right = self.right.simplify()
@@ -171,6 +189,14 @@ class Multiply(Expression):
             return Multiply(right, left).simplify()
         if isinstance(left, Constant) and isinstance(right, Multiply) and isinstance(right.left, Constant):
             return Multiply(left * right.left, right.right).simplify()
+        if isinstance(left, Minus) and isinstance(right, Minus):
+            return Multiply(left.value, right.value).simplify()
+        if isinstance(left, Minus) and not isinstance(right, Minus):
+            return Minus(Multiply(left.value, right).simplify())
+        if isinstance(right, Minus) and not isinstance(left, Minus):
+            return Minus(Multiply(left, right.value).simplify())
+        if isinstance(right, Power) and isinstance(left, Power) and (right.base == left.base):
+            return (right.base ** (right.exponent + left.exponent)).simplify()
         return Multiply(left, right)
 
     def __repr__(self):
@@ -200,12 +226,40 @@ class Add(Expression):
             return right
         if right == 0:
             return left
+        if isinstance(left, Constant) and isinstance(right, Constant):
+            return Constant(left.value + right.value)
         return left + right
+
+@dataclass
+class Minus(Expression):
+    value: Expression
+
+    def simplify(self):
+        value = self.value.simplify()
+        if isinstance(self.value, Constant):
+            return Constant(-1 * value)
+        return Minus(value)
+
+    def __repr__(self):
+        if isinstance(self.value, Constant):
+            return f'-{self.value}'
+        else:
+            return f'- {self.value}'
+
 
 @dataclass
 class Subtract(Expression):
     left: Any
     right: Any
+
+    def simplify(self):
+        left = self.left.simplify()
+        right = self.right.simplify()
+        if left == 0:
+            return Minus(right)
+        if right == 0:
+            return left
+        return left - right
 
     def __repr__(self):
         """
@@ -216,20 +270,77 @@ class Subtract(Expression):
 
 @dataclass
 class Divide(Expression):
-    left: Any
-    right: Any
+    dividend: Any
+    divisor: Any
+
+    def simplify(self):
+        dividend = self.dividend.simplify()
+        divisor = self.divisor.simplify()
+        if isinstance(dividend, Constant) and isinstance(divisor, Constant):
+            # todo: gcd?
+            if dividend.value % divisor.value == 0:
+                return Constant(dividend.value / divisor.value)
+            if divisor.value % dividend.value == 0:
+                return Divide(Constant(1), Constant(divisor.value / dividend.value))
+        if isinstance(dividend, Constant) and isinstance(divisor, Variable):
+            return (dividend * (divisor ** (-1))).simplify()
+        if isinstance(dividend, Minus) and isinstance(divisor, Minus):
+            return Divide(dividend.value, divisor.value).simplify()
+        if isinstance(dividend, Minus) and not isinstance(divisor, Minus):
+            return Minus(Divide(dividend.value, divisor).simplify())
+        if not isinstance(dividend, Minus) and isinstance(divisor, Minus):
+            return Minus(Divide(dividend, divisor.value).simplify())
+        return dividend / divisor
 
     def __repr__(self):
         """
         >>> print(Divide(Constant(2), Variable('x')))
         2 / x
         """
-        return f'{self.left} / {self.right}'
+        return f'{self.dividend} / {self.divisor}'
+
+@dataclass
+class Logarithm(Expression):
+    value: Expression
+    base: int = math.e
+
+    def simplify(self):
+        value = self.value.simplify()
+        base = self.base
+        if value == base:
+            return Constant(1)
+        else:
+            return Logarithm(value, base)
+
+    def __repr__(self):
+        """
+        >>> print(Logarithm(10, math.e))
+        ln(10)
+        >>> print(Logarithm(2, 3))
+        log(2, 3)
+        """
+        if self.base == math.e:
+            return f'ln({self.value})'
+        else:
+            return f'log({self.value}, {self.base})'
 
 @dataclass
 class Power(Expression):
     base: Any
     exponent: Any
+
+    def simplify(self):
+        """
+        >>> Power(Add(Constant(2), Constant(0)), Constant(2)).simplify()
+        4
+        """
+        base = self.base.simplify()
+        exponent = self.exponent.simplify()
+        if isinstance(base, Constant) and isinstance(exponent, Constant):
+            return Constant(base.value ** exponent.value)
+        if isinstance(exponent, Constant) and exponent == 1:
+            return base
+        return Power(base, exponent)
 
     def __repr__(self):
         """
@@ -240,6 +351,7 @@ class Power(Expression):
 
 def derive(expr: Expression, var: Variable) -> Expression:
     """
+    >>> from math import e
     >>> x = Variable('x')
     >>> y = Variable('y')
     >>> derive(Constant(3), x)
@@ -254,6 +366,18 @@ def derive(expr: Expression, var: Variable) -> Expression:
     2
     >>> derive(2 * x ** 3 + 3 * x + 2, x)
     6 * x ** 2 + 3
+    >>> derive(2 * x ** 3 - 3 * x + 2, x)
+    6 * x ** 2 - 3
+    >>> derive(x / 2, x)
+    1 / 2.0
+    >>> derive(2 / x, x)
+    - 2 / x ** 2
+    >>> derive(x ** 3, x)
+    3 * x ** 2
+    >>> derive(3 ** x, x)
+    3 ** x * ln(3)
+    >>> derive(e ** x, x)
+    e ** x
     """
 
     expr = expr.simplify()
@@ -271,8 +395,15 @@ def derive(expr: Expression, var: Variable) -> Expression:
     if isinstance(expr, Subtract):
         return (derive(expr.left, var) - derive(expr.right, var)).simplify()
     if isinstance(expr, Divide):
-        return (((derive(expr.left, var) * expr.right) - (expr.left * derive(expr.right, var))) / (expr.right ** 2)).simplify()
+        return (((derive(expr.dividend, var) * expr.divisor) - (expr.dividend * derive(expr.divisor, var))) / (expr.divisor ** 2)).simplify()
+    if isinstance(expr, Logarithm):
+        return ((1 / expr.value) * derive(expr.value, var)).simplify()
     if isinstance(expr, Power):
+        return (derive(expr.exponent * Logarithm(expr.base), var) * expr).simplify()
+        """
         if isinstance(expr.exponent, Constant) and isinstance(expr.base, Variable) and expr.base == var:
-            return expr.exponent * expr.base ** Constant(expr.exponent.value - 1)
+            return (expr.exponent * expr.base ** Constant(expr.exponent.value - 1)).simplify()
+        if isinstance(expr.base, Constant) and isinstance(expr.exponent, Variable) and expr.exponent == var:
+            return ((expr.base.value ** expr.exponent) * Logarithm(expr.base)).simplify()
+        """
     raise Exception('Derivation not implemented for: ' + str(expr))
